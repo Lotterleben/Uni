4a30364b30ffe230f9607f8ec81f18b7dabe14c5
Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
2014-11-21 11:05:52 +0100

    + starting first steps

--- TT1/TTV_CHORD/src/Chord_Battleship/Chord_Battleship.java
+++ TT1/TTV_CHORD/src/Chord_Battleship/Chord_Battleship.java
@@ -1,12 +1,51 @@
 package Chord_Battleship;
 
+import java.net.MalformedURLException;
+
 import de.uniba.wiai.lspi.chord.data.ID;
+import de.uniba.wiai.lspi.chord.data.URL;
+import de.uniba.wiai.lspi.chord.service.NotifyCallback;
 import de.uniba.wiai.lspi.chord.service.PropertiesLoader;
+import de.uniba.wiai.lspi.chord.service.ServiceException;
 import de.uniba.wiai.lspi.chord.service.impl.ChordImpl;
 
-public class Chord_Battleship implements de.uniba.wiai.lspi.chord.service.NotifyCallback{
+public class Chord_Battleship implements NotifyCallback{
 
 	private ChordImpl chord = null;
+	
+	public void startServer(String serverURL) {
+		PropertiesLoader.loadPropertyFile();
+		chord = new ChordImpl();
+		chord.setCallback(this);
+		try {
+			URL url = new URL(serverURL);
+			chord.create(url);
+		} catch (ServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void startClient(String client, String server) {
+		PropertiesLoader.loadPropertyFile();
+		chord = new ChordImpl();
+		chord.setCallback(this);
+		try {
+			URL clientURL = new URL(client);
+			URL bootstrapURL = new URL(server);
+			chord.join(clientURL, bootstrapURL);
+		} catch (ServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	@Override
 	public void retrieved(ID target) {
 		System.out.println("retrieved " + target);
@@ -19,12 +58,6 @@ public class Chord_Battleship implements de.uniba.wiai.lspi.chord.service.Notify
 		System.out.println("broadcast ; source: " + source +" target: " + "hit: " + hit);
 		// TODO Auto-generated method stub
 		
-	}
-	
-	public void start (){
-		PropertiesLoader.loadPropertyFile();
-		chord = new ChordImpl();
-		chord.setCallback(this);
 	}
 
 }
--- TT1/TTV_CHORD/src/Chord_Battleship/Main.java
+++ TT1/TTV_CHORD/src/Chord_Battleship/Main.java
@@ -1,9 +1,14 @@
 package Chord_Battleship;
 
 public class Main {
+	// caution, these IPs are bullshit
+	static String serverURL = "ocsocket://141.22.56.21:123456";
+	static String clientURL = "ocsocket://141.22.56.21:123456";
 
+	
 	public static void main(String[] args) {
 		Chord_Battleship chord = new Chord_Battleship();
-		chord.start();
+		chord.startServer(serverURL);
+		chord.startClient(clientURL, serverURL);
 	}
 }
