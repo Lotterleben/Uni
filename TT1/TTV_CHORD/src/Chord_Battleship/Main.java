f012e8a97de5d421ae0ddc9a38268b0c2a75ae65
Lotte Steenbrink <lotte@zombietetris.de>
2014-12-12 12:11:27 +0100

    code cleanup

--- TT1/TTV_CHORD/src/Chord_Battleship/Main.java
+++ TT1/TTV_CHORD/src/Chord_Battleship/Main.java
@@ -1,14 +1,20 @@
 package Chord_Battleship;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+
 public class Main {
 	// caution, these IPs are bullshit
-	static String serverURL = "ocsocket://141.22.56.21:12345/";
-	static String clientURL = "ocsocket://141.22.56.21:12345/";
+	static String serverURL = "ocsocket://141.22.28.180:12345/";
+	static String clientURL = "ocsocket://141.22.28.170:12345/";
 
 	
 	public static void main(String[] args) {
+		// make logger STFU
+		LogManager.getRootLogger().setLevel(Level.INFO);
+		
 		Chord_Battleship chord = new Chord_Battleship();
-		chord.startServer(serverURL);
-		//chord.startClient(clientURL, serverURL);
+		//chord.startServer(serverURL);
+		chord.startClient(clientURL, serverURL);
 	}
 }
