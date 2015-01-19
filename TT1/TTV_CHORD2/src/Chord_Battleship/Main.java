7ee111d8319ae3c01ba7edb571293342a3a0541f
Lotte Steenbrink <lotte@zombietetris.de>
2015-01-19 19:25:08 +0100

    merp

---
+++ TT1/TTV_CHORD2/src/Chord_Battleship/Main.java
@@ -0,0 +1,62 @@
+package Chord_Battleship;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import de.uniba.wiai.lspi.chord.service.PropertiesLoader;
+
+public class Main {
+
+	static String serverURL = "ocsocket://127.0.0.1:12350/";
+	static String baseClientURL = "ocsocket://127.0.0.1:1235";
+	static int numClients = 3;
+	
+	static List<Strategy> nodes = new ArrayList<Strategy>();
+	
+	public static void main(String[] args) {
+		PropertiesLoader.loadPropertyFile();
+		
+		System.out.println("Starting server at" + serverURL);
+		Strategy serverNode = new Strategy();
+		serverNode.startServer(serverURL);
+		nodes.add(serverNode);
+		
+		launchClients();
+		
+		System.out.println("\nAll clients launched. Press enter to start the battle:\n");
+		Scanner in = new Scanner(System.in);
+		in.nextLine();
+		in.close();
+		
+		for(Strategy node : nodes) {
+			node.initializeNode();
+		}
+		
+		System.out.println("Begun, the battleship wars have.");
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		serverNode.test();
+		
+		
+
+	}
+	
+	public static void launchClients() {
+		String clientURL;
+		for (int i=1; i<= numClients; i++) {
+			clientURL = baseClientURL+i+"/";
+			System.out.println("Starting client at: "+clientURL);
+			
+			Strategy clientNode = new Strategy();
+			clientNode.startClient(clientURL, serverURL);
+			nodes.add(clientNode);
+		}
+	}
+
+}
